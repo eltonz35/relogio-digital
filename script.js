b232dc1553faf2e90b1eb53edb952d4b688a4e34
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-23 15:47:51 -0300

    criando funcao corrigirHorario

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ function atualizarTempo() {
     let tempo = new Date();
     
     let horario = tempo.getHours() + ":" + tempo.getMinutes();
-    
+
     display.textContent = horario;
 }
 
+function corrigirHorario() {
+    if(numero < 10) {
+        numero = "0" + numero;
+    }
+    return numero;
+}
 
 setInterval(atualizarTempo, 1000);
